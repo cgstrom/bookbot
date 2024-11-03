7086a194570672a3026eb79145cf9045e9e8bb72
Chris Strom <cstrom@clearpivot.com>
2024-11-02 17:44:37 -0700

    add functions to main.py

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print("hello world")
+def main():
+    with open("books/frankenstein.txt") as f:
+        file_contents = f.read()
+        #print(file_contents)
+
+        word_count(file_contents)
+
+def word_count(file_contents):
+    count = 0
+    words = file_contents.split()
+    #print(words)
+    for word in words:
+        count += 1
+    print(count)
+
+
+
+main()
